dc8e7193bc46901e11d7747856f943f3a8249959
Kriti Paruchuri <151987623+kri0211@users.noreply.github.com>
2025-12-20 20:37:53 +0530

    Delete Severity_based_pothole_detection directory

--- Severity_based_pothole_detection/check.py
+++
@@ -1,9 +0,0 @@
-import os
-
-folder = r"dataset\labels\val"
-for file in os.listdir(folder):
-    path = os.path.join(folder, file)
-    with open(path, 'r') as f:
-        content = f.read().strip()
-        if content == "":
-            print("Empty label:", file)
